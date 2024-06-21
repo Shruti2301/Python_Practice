d60b8dcf403c2bf525862e1f27871e1df3fd7820
Shruti Mandaokar <shruti.mandaokar@gmail.com>
2024-06-21 02:01:44 -0700

    Create minimaxsum.py

---
+++ minimaxsum.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+# https://www.hackerrank.com/challenges/mini-max-sum/problem
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Sort the array
+    arr.sort()
+    
+    # Calculate the minimum sum (sum of the first four elements)
+    min_sum = sum(arr[:4])
+    
+    # Calculate the maximum sum (sum of the last four elements)
+    max_sum = sum(arr[-4:])
+    
+    # Print the results
+    print(min_sum, max_sum)
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
